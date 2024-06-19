da2eb7d4049e2727aced4453c029484efc5930d3
Erin <ball0@unbc.ca>
2024-06-19 16:27:01 -0700

    push for eod

--- ladder-one-half-dmrg.jl
+++ ladder-one-half-dmrg.jl
@@ -4,9 +4,9 @@ using Random
 
 Random.seed!(6969)
 
-function dmrgLadder(length, jl, jr, operator=-1)
+function dmrgLadder(numPairs, jl, jr, operator=-1)
 	
-	data = getOperator(length, jl, jl, operator)
+	data = getOperator(numPairs, jl, jr, operator)
 	op = data[1]
 	sites = data[2]
 	
@@ -24,13 +24,13 @@ function dmrgLadder(length, jl, jr, operator=-1)
 	return [energy, psi]
 end
 
-function getOperator(length, jl, jr, operator)
+function getOperator(numPairs, jl, jr, operator)
 	if operator == -1
-		return hamiltonian(length, jl, jr)
+		return hamiltonian(numPairs, jl, jr)
 	end
 	if typeof(operator) <: Array
-		if length(operator) >= 2
-			return#TODO return something
+		if true #(length(operator) <= 4)
+			return siteOperator(numPairs, data)
 		else
 			return ArgumentError("operator unrecognised e#01")
 		end
@@ -38,8 +38,8 @@ function getOperator(length, jl, jr, operator)
 	return ArgumentError("operator unrecognised e#02")
 end
 
-function hamiltonian(length, jl, jr)
-	N = length*2
+function hamiltonian(numPairs, jl, jr)
+	N = numPairs*2
 	Jl = jl		#inter dimer interaction
 	Jr = jr		#intra dimer interaction
 			#we are testing limit Jr >> Jl
@@ -66,18 +66,38 @@ function hamiltonian(length, jl, jr)
 	return [op, sites]
 end
 
-function siteOperator(opMatrix)
-	operators = opMatrix[1]
-	opdSites = opMatrix[2]
+function siteOperator(numPairs, opMatrix)
+	operators1 = opMatrix[1]
+	operators2 = opMatrix[2]	#TODO should i assert that operators1 and operators2 are the same length? probably :S
+	siteSets1 = opMatrix[3]
+	siteSets2 = opMatrix[4]
 	
-	siteSets = length(opdSites)
+	sites = siteinds("S=1/2", numPairs*2)
+	os = OpSum()
+	numSiteSets = length(siteSets1)
 	
-	if siteSets < length(operators)
+	if numSiteSets < length(operators1)
 		print("WARNING: one or more operator sets will be ignored as they have no corresponding site sets\n")
 	end
-	for i in 1:siteSets
-		#TODO apply op !	
+	for i in 1:numSiteSets
+		if isempty(operators2[1])
+			operators2[i+1] = "I"
+		end
+		if isempty(operators1[i])
+			operators1[i] = "I"
+		end
+		#get site op for this set
+		for j in 1:length(siteSets1[i])	#TODO implement constant multiplication later
+			print("hew")
+			os += operators1[i], siteSets1[j], operators2[i], siteSets2[j]
+		end	
 	end
+	
+	op = MPO(os,sites)
+	
+	return [op, sites]
 end
 
-dmrgLadder(1,1,1)
+data = [("Sz",),("I",),(1,2),(1,2)]
+
+display(dmrgLadder(2,1,1,data)[1])
