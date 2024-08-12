0775ab0038a1bff10a513efe944722c851fd3f25
Erin <ball0@unbc.ca>
2024-08-12 14:22:29 -0700

    not working but this is what i expect to use

--- dmrg/ladder.jl
+++ dmrg/ladder.jl
@@ -41,4 +41,57 @@ function dmrgLadder(numPairs, Jl, Jr)
 	return [energy, psi, sites]
 end
 
+function timeEvolutionChain(numSites, Jl, Jr)
+  N = numSites
+  cutoff = 1E-8
+  tau = 0.1
+  ttotal = 5.0
 
+  # Make an array of 'site' indices
+  s = siteinds("S=1/2", N; conserve_qns=true)
+
+  # Make gates (1,2),(2,3),(3,4),...
+  gates = ITensor[]
+  for j in 1:2:(N - 3)
+    s1 = s[j]
+    s2 = s[j + 1]
+    s3 = s[j + 2]
+    s4 = s[j + 3]
+    hj =
+      Jr / 2 * (op("S-", s1) * op("S+", s2) + op("S+", s1) * op("S-", s2) + 
+                op("S-", s3) * op("S+", s4) + op("S+", s3) * op("S-", s4) +
+                op("Sz", s1) * op("Sz", s2) + op("Sz", s3) * op("Sz", s4))+
+      Jl / 2 * (op("S-", s1) * op("S+", s3) + op("S+", s1) * op("S-", s3) + 
+                op("S-", s2) * op("S+", s4) + op("S+", s2) * op("S-", s4) +
+                op("Sz", s1) * op("Sz", s3) + op("Sz", s2) * op("Sz", s4))
+      #op("Sz", s1) * op("Sz", s2) +
+      #1 / 2 * op("S+", s1) * op("S-", s2) +
+      #1 / 2 * op("S-", s1) * op("S+", s2)
+    Gj = exp(-im * tau / 2 * hj)
+    push!(gates, Gj)
+  end
+  # Include gates in reverse order too
+  # (N,N-1),(N-1,N-2),...
+  append!(gates, reverse(gates))
+
+  # Initialize psi to be a product state (alternating up and down)
+  psi = MPS(s, n -> isodd(n) ? "Up" : "Dn")
+
+  c = div(N, 2) # center site
+
+  # Compute and print <Sz> at each time step
+  # then apply the gates to go to the next time
+  for t in 0.0:tau:ttotal
+    Sz = expect(psi, "Sz"; sites=c)
+    println("$t $Sz")
+
+    t≈ttotal && break
+
+    psi = apply(gates, psi; cutoff)
+    normalize!(psi)
+  end
+
+  return
+end
+
+timeEvolutionChain(10,1,1)
